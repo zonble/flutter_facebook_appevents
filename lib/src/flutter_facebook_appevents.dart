e204c668add73813f51fb2b4629653fecfb843ea
zonble <zonble@gmail.com>
2024-09-28 20:08:53 +0800

    Updates pubspec.

--- lib/src/flutter_facebook_appevents.dart
+++ lib/src/flutter_facebook_appevents.dart
@@ -6,7 +6,7 @@ import 'package:flutter/services.dart';
 /// Bridges Facebook SDK's App Events functionality.
 class FacebookAppEvents {
   static const MethodChannel _channel =
-  const MethodChannel('flutter_facebook_appevents');
+      MethodChannel('flutter_facebook_appevents');
 
   /// Resets user ID.
   static Future<void> resetUserId() async =>
@@ -18,7 +18,7 @@ class FacebookAppEvents {
 
   /// Log an event with [eventName] and [parameters].
   static Future<void> logEvent(String eventName, Map parameters) async {
-    Map map = {'event_name': eventName, 'parameters': parameters ?? {}};
+    Map map = {'event_name': eventName, 'parameters': parameters};
     String jsonString = json.encode(map);
 
     await _channel.invokeMethod('set_user_id', jsonString);
